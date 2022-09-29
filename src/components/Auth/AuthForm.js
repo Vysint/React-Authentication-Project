43adeac39ab4e78a952656d8164c7ad9b6bf8df7
Vincent Kipngetich <vinniebluz@icloud.com>
2022-09-29 12:42:51 +0300

    Showed feedback to the user

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -7,6 +7,7 @@ const AuthForm = () => {
   const passwordInputRef = useRef();
 
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -18,6 +19,7 @@ const AuthForm = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
+    setIsLoading(true);
     if (isLogin) {
     } else {
       fetch(
@@ -33,12 +35,16 @@ const AuthForm = () => {
             "Content-Type": "application/json",
           },
         }
-      ).then(res => {
-        if (res.ok){
-
-        }else{
+      ).then((res) => {
+        setIsLoading(false);
+        if (res.ok) {
+        } else {
           return res.json().then((data) => {
-            console.log(data);
+            let errorMessage = "Authentication Failed!";
+            if (data && data.error && data.error.message) {
+              errorMessage = data.error.message;
+            }
+            alert(errorMessage);
           });
         }
       });
@@ -62,7 +68,8 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
+          {!isLoading && <button>{isLogin ? "Login" : "Create Account"}</button>}
+          {isLoading && <p>Sending request...</p>}
           <button
             type="button"
             className={classes.toggle}
